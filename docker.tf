216cb76f9564dd849342046736acdece37f64d7c
prudh <prudhhviraj2@gmail.com>
2025-07-06 20:12:29 +0530

    Add previously untracked files

---
+++ docker.tf
@@ -0,0 +1,48 @@
+resource "aws_instance" "this"{
+    ami                    = "ami-09c813fb71547fc4f"
+    vpc_security_group_ids = [aws_security_group.allow_tls.id]
+    instance_type          = "t3.micro"
+    user_data = <<-EOF
+            #!/bin/bash
+            sudo dnf update -y
+            sudo dnf install -y dnf-utils
+            sudo dnf -y install dnf-plugins-core
+            sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+            sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+            sudo systemctl enable --now docker
+            sudo usermod -aG docker ec2-user
+            EOF
+    tags = {
+        Name = "docker"
+    }
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+/*     ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] 
+    } */
+    ingress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"] 
+    }
+  
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  tags = {
+    Name = "security_tls"
+  }
+}
