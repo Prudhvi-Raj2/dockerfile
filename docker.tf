713a311e35ce7cc1210b5b4c2b4bc900fc8d6cb8
prudh <prudhhviraj2@gmail.com>
2025-07-09 08:41:40 +0530

    ARGS files has been added

--- docker.tf
+++ docker.tf
@@ -2,17 +2,7 @@ resource "aws_instance" "this"{
     ami                    = "ami-09c813fb71547fc4f"
     vpc_security_group_ids = [aws_security_group.allow_tls.id]
     instance_type          = "t3.micro"
-    user_data = <<-EOF
-            #!/bin/bash
-            sudo dnf update -y
-            sudo dnf install -y dnf-utils
-            sudo dnf -y install dnf-plugins-core
-            sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-            sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-            # sudo systemctl enable --now docker
-            sudo systemctl enable --now docker
-            sudo usermod -aG docker ec2-user
-            EOF
+    user_data = file("${path.module}/install_docker.sh")
     tags = {
         Name = "docker"
     }
---
+++ install_docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo dnf update -y
+sudo dnf install -y dnf-utils
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable --now docker
+sudo usermod -aG docker ec2-user
